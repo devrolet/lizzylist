bb461caa2725f53e1060f735addff75c6f5c1671
Chet Hill <chetdhill@gmail.com>
2018-09-19 22:00:14 -0400

    Unlock HTTP Client

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
 import 'rxjs/add/operator/map';
 import { RecipeService } from 'src/app/shared/recipe.service';
 import { Recipe } from 'src/app/recipe-book/recipe.model';
@@ -8,24 +8,23 @@ import { AuthService } from 'src/app/auth/auth.service';
 @Injectable()
 export class DataStorageService {
 
-  constructor(private http: Http,
+  constructor(private httpClient: HttpClient,
               private recipeService: RecipeService,
               private authService: AuthService) { }
 
   storeRecipes() {
     const token = this.authService.getToken();
 
-    return this.http.put('Firebase Link Here' + token, this.recipeService.getRecipes());
+    return this.httpClient.put('Firebase Link Here' + token, this.recipeService.getRecipes());
   }
 
   getRecipes() {
     const token = this.authService.getToken();
 
-    this.http.get('Firebase Link Here' + token)
+    this.httpClient.get<Recipe[]>('Firebase Link Here' + token)
       .map(
-        (response: Response) => {
-          const recipes: Recipe[] = response.json();
-          for (let recipe of recipes) {
+        (recipes) => {
+          for (const recipe of recipes) {
             if (!recipe['ingredients']) {
               console.log(recipe);
               recipe['ingredients'] = [];
